e0ac2f292e7444691d4f0c2fea90ed908126672d
Bruno <brunoacosta903@gmail.com>
2025-04-25 20:11:11 +0200

    Problematic inheritance

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,16 +1,57 @@
-(()=>{
+(() => {
 
 
     type Gender = 'M' | 'F';
-    class Person{
+    class Person {
 
         constructor(
-            public name: string, 
-            public gender: Gender, 
-            public birthday: Date){}
+            public name: string,
+            public gender: Gender,
+            public birthday: Date
+        ) { }
     }
 
-    const newPerson = new Person('Fernando','M',new Date('1985-10-21'));
-    console.log(newPerson);
+    class User extends Person {
+        public lastAccess: Date;
+        constructor(
+            public email: string,
+            public role: string,
+            public name: string,
+            public gender: Gender,
+            public birthday: Date
+        ) {
+            super(name, gender, birthday);
+            this.lastAccess = new Date()
+        }
+
+        checkCredentials(): boolean {
+            return true;
+        }
+
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenedFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthday: Date
+        ) { super(email, role, name, gender, birthday) }
+    }
+
+    const userSettings = new UserSettings(
+        "/usr/home",
+        "/home",
+        "/fernando@google.com",
+        "Admin",
+        "Fernando",
+        "M",
+        new Date('1985-10-21')
+    );
+
+    console.log({ userSettings, areCredentialsValid: userSettings.checkCredentials() });
 
 })();
