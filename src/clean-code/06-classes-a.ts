586c5fd1d4615d24e76bc98edbcc960078cbeed0
Bruno <brunoacosta903@gmail.com>
2025-04-25 19:57:32 +0200

    Brief introduction to classes

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,16 @@
+(()=>{
+
+
+    type Gender = 'M' | 'F';
+    class Person{
+
+        constructor(
+            public name: string, 
+            public gender: Gender, 
+            public birthday: Date){}
+    }
+
+    const newPerson = new Person('Fernando','M',new Date('1985-10-21'));
+    console.log(newPerson);
+
+})();
